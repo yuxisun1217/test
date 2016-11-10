aea03a006a1c6873150d37f98cc5b7a2a730bec8
Yuxin Sun <yuxisun@redhat.com>
2016-11-10 17:19:27 +0800

    parse config.yaml

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import time
+import yaml
+with open("config.yaml") as f:
+    p=yaml.load(f)
 timestamp=time.strftime("%Y%m%d %H:%M:%S", time.localtime())
+prefix="[%s] " % timestamp
+string=prefix+"Project: %(project)s RHEL: %(rhel_version)s\n" % p
 with open("/tmp/test.log",'a') as f:
-    f.write(timestamp+'\n')
+    f.write(string)
